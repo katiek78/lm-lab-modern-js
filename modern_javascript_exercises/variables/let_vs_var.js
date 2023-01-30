0ddfaa1acbe211c71b0222d9b6a362b9cb88a8c5
katiek78 <kermodekatie@gmail.com>
2023-01-30 14:11:46 +0000

    Completed variables.js

--- modern_javascript_exercises/variables/let_vs_var.js
+++ modern_javascript_exercises/variables/let_vs_var.js
@@ -9,11 +9,11 @@ function count() {
   console.log(i + " outside the forLoop");
 }
 
-// count(); // <---- uncomment me!
+count();
 
 // Another problem with var is that we can overwrite it, like this:
 
-var greeting = "Hello 😘";
-var greeting = "Not today, pal! 😤";
+let greeting = "Hello 😘";
+// let greeting = "Not today, pal! 😤";
 
-// console.log(greeting) // <----- uncomment me!
+console.log(greeting);
